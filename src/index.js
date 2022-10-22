b4350dbd16e435c46618143f1f1c2b4f82954f84
Jaqueline Cantarino <jaquelinecantarinoc@gmail.com>
2022-10-21 21:23:32 -0300

    base primereact

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import 'primereact/resources/primereact.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
+import 'primereact/resources/themes/bootstrap4-light-blue/theme.css'
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
